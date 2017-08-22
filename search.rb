2167c9e7d7c063934ae80eacd3c2829a20fb8135
Kat Gifford <kat@pancakecreative.com>
2017-08-22 12:57:34 -0700

    refactor search and it works for exact match

--- search.rb
+++ search.rb
@@ -3,34 +3,22 @@ require_relative 'session_setup'
 @client = redboothInit
 
 userArg = ARGV.join(" ")
+# userArg = "PM"
 resultsHash = {"items": []}
 
-# results = @client.search(query: userArg)
-resultsRaw = @client.search(query: 'test with john 1')
-results = resultsRaw.response.data
-
-# .find method does not exist on var searchexit
-
-# search.find {|i| i['title'] == 'test with john 1'}
+results = @client.search.response.data
 
 results.each do |i|
-  results_obj = {
-    		"type": i['target_type'],
-    		"title": i['title'],
-    		"subtitle": "show '#{i['title']}'",
-        "parent_id": i['parent_id'],
-    		"arg": i['target_id']
-      }
-  resultsHash[:items].push(results_obj)
-end
-
-searchResults = {"items": []}
-binding.pry
-
-resultsHash.each do |i|
-  if i["title"].include?("test with john 1")
-    searchResults.push(i)
+  if i["title"] == userArg
+    results_obj = {
+          "type": i['target_type'],
+          "title": i['title'],
+          "subtitle": "show '#{i['title']}'",
+          "parent_id": i['parent_id'],
+          "arg": i['target_id']
+        }
+    resultsHash[:items].push(results_obj)
   end
 end
 
-puts JSON.generate(searchResults)
+puts JSON.generate(resultsHash)
